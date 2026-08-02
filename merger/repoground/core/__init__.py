6a58b071ac6f7812d9102d099c2bab768f3b251a
Alexander Mohr <alexdermohr@gmail.com>
2026-08-02 07:58:09 +0200

    feat: expose explicit SCIP adapter core API

--- merger/repoground/core/__init__.py
+++ merger/repoground/core/__init__.py
@@ -1,1 +1,15 @@
+from merger.repoground.core.scip_adapter import (
+    ScipAdapterError,
+    benchmark_identity,
+    evaluate_scip_adapter,
+    normalize_scip_index,
+)
+
 __core_version__ = "2.4.0"
+
+__all__ = [
+    "ScipAdapterError",
+    "benchmark_identity",
+    "evaluate_scip_adapter",
+    "normalize_scip_index",
+]
